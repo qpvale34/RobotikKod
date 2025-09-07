9aa3177e4bee7734e93e0967755e658dcdcd2a52
qpvale34 <benledance@gmail.com>
2025-09-07 04:44:29 -0700

    Çakış

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,17 @@ import path from "path";
 
 export default defineConfig({
   base: '/RobotikKod/', // repo adı ile aynı olmalı
-
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.dirname, "client", "src"),
-      "@shared": path.resolve(import.meta.dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+      "@": path.resolve(__dirname, "client", "src"),
+      "@shared": path.resolve(__dirname, "shared"),
+      "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
-  root: path.resolve(import.meta.dirname, "client"),
+  root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
   },
   server: {
